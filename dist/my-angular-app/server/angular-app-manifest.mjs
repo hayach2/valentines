1d309a10dd9a2f33f8c443937b0f40357609f5c6
Haya Ch2 <hayach2@Hayas-MBP.mynet>
2026-02-05 16:16:22 +0000

    update

--- dist/my-angular-app/server/angular-app-manifest.mjs
+++ dist/my-angular-app/server/angular-app-manifest.mjs
@@ -20,10 +20,10 @@ export default {
 ],
   entryPointToBrowserMapping: undefined,
   assets: {
-    'index.csr.html': {size: 4206, hash: '8458049035afc1de1e72b8bf5213537e8f22d95b5625d7ce6b281e0b8d6094cb', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
-    'index.server.html': {size: 981, hash: '684306c21c4c9c8f20795c938fd0824b5f5df613ed56102480232caa5ac9a813', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
-    'yes/index.html': {size: 5908, hash: '9cf35bb1794533c1730e17f5a03411c192496ee8bc5dfa3001eb3a780aee26c9', text: () => import('./assets-chunks/yes_index_html.mjs').then(m => m.default)},
-    'index.html': {size: 9601, hash: 'd6f94da2bb70708d6c1f0b99275d2e444f81e0fd2d1e7c2c2ce6f9ac64e053cc', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
-    'styles-OT6FAHWG.css': {size: 10428, hash: 'P9pe7ff+i3w', text: () => import('./assets-chunks/styles-OT6FAHWG_css.mjs').then(m => m.default)}
+    'index.csr.html': {size: 5686, hash: '8489a04bb285f8d28313310fea2af394d9ed22202dd696e8a197fcde20465245', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
+    'index.server.html': {size: 981, hash: '827873ade735ba8775366222ceac1a07afc3f339697b1c52297dd30533cf73b2', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
+    'yes/index.html': {size: 7464, hash: 'a71ef6bb9934f70ea158682021ab72e1846e8f6fb0ce0506a50ffe51a5668c3f', text: () => import('./assets-chunks/yes_index_html.mjs').then(m => m.default)},
+    'index.html': {size: 11157, hash: 'f36be5337bad751bb1e5b6cf222b8046efd15c4f1f27c55888a341c53b9c18df', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
+    'styles-O5ODYPRE.css': {size: 13439, hash: '2Fpq3YQCOsQ', text: () => import('./assets-chunks/styles-O5ODYPRE_css.mjs').then(m => m.default)}
   },
 };
